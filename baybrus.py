238ce4b06903b0bad69a384301b53dfbc5eb4684
Muneeb <muneebrizwan20@gmail.com>
2024-06-14 14:37:52 +0500

    Recognizer completed

--- baybrus.py
+++ baybrus.py
@@ -1,8 +1,13 @@
 #libraries-----------------------------------------------------------------
 import pyttsx3
-import SpeechRecognition
+import speech_recognition as sr
 import time
+#variables-----------------------------------------------------------------
+r = sr.Recognizer()
+keywords = [("baybrus",1),("hey baybrus",1)]
+source = sr.Microphone()
 #functions-----------------------------------------------------------------
+# speaks-------------------------------------------------------------------
 def Speak(text):
     rate = 100;
     engine = pyttsx3.init()
@@ -11,6 +16,41 @@ def Speak(text):
     engine.setProperty('rate', rate+50)
     engine.say(text)
     engine.runAndWait()
+# Callback function---------------------------------------------------------
+def callback(recognizer,audio):
+    try:
+        speech_as_text = recognizer.recognizer_sphinx(audio, keyword_entries=keywords)
+        print(speech_as_text)
+        if "Baybrus" in speech_as_text or "hi baybrus" :
+            Speak("hello sir ?")
+            recognizer_main()
+    except sr.UnknownValueError:
+        print("Oops didn't catch up with you buddy !")
+# start Recognizer-------------------------------------------------------------
+def start_recognizer():
+    print("Waiting you for buddy")
+    r.listen_in_background(source,callback)
+    time.sleep(1000000)
+# Main function for speech recognition-----------------------------------------
+def recognizer_main():
+    with sr.Microphone() as source:
+        print("Say Something")
+        audio = r.listen(source)
+        data = ""
+        try:
+            data = r.recognize_google(audio).lower()  # Correct method usage here
+            print("You said: " + data)
+            # Respond to different commands
+            if "how are you" in data:
+                Speak("I am fine")
+            elif "hello" in data:
+                Speak("What's up buddy")
+            else:
+                Speak("I am sorry sir")
+        except sr.UnknownValueError:
+            print("Baybrus did not understand your input")
+        except sr.RequestError as e:
+            print("Couldn't understand: {0}".format(e))
 #mainprogram---------------------------------------------------------------
-Speak("HelloWorld")
-Speak("Fuck You")
+while 1:
+    start_recognizer()

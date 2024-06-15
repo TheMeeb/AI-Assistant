78747c0b2328030d76655e7bcfda6bcddeb92859
Muneeb <muneebrizwan20@gmail.com>
2024-06-15 15:04:01 +0500

    too close

--- baybrus.py
+++ baybrus.py
@@ -2,6 +2,8 @@
 import pyttsx3
 import speech_recognition as sr
 import time
+import random 
+from openpyxl import *
 #variables-----------------------------------------------------------------
 r = sr.Recognizer()
 keywords = [("baybrus",1),("hey baybrus",1)]
@@ -38,19 +40,41 @@ def recognizer_main():
         audio = r.listen(source)
         data = ""
         try:
-            data = r.recognize_google(audio).lower()  # Correct method usage here
+            data = r.recognize_google(audio).lower()  # Correct method usage here--------------
             print("You said: " + data)
-            # Respond to different commands
-            if "how are you" in data:
-                Speak("I am fine")
-            elif "hello" in data:
-                Speak("What's up buddy")
+            # Respond to different commands-------------------------------------
+            if data in hello_list:
+                Speak(random.choice(reply_hello))
+                time.sleep(2)
+            elif data in how_are_you:
+                Speak(random.choice(reply_hello_are_you))
+                time.sleep(2)
             else:
                 Speak("I am sorry sir")
         except sr.UnknownValueError:
             print("Baybrus did not understand your input")
         except sr.RequestError as e:
             print("Couldn't understand: {0}".format(e))
+# Excel response function ------------------------------------------------------
+def excel():
+    wb = load_workbook("file.xlsx")
+    wu = wb.get_sheet_by_name("User")
+    wr = wb.get_sheet_name("Replies")
+
+    global Hello_list
+    global how_are_you
+    urow1 = wu['1']
+    urow2 = wu['2']
+    hello_list = [urow1[x].value for x in range(lens(urow1))]
+    how_are_you = [urow2[x].value for x in range(lens(urow2))]
+    
+    global reply_hello_list
+    global reply_how_are_you
+    rrow1 = wr['1']
+    rrow2 = wr['2']
+    reply_hello_list= [urow1[x].value for x in range(lens(urow1))]
+    reply_hello_list= [urow2[x].value for x in range(lens(urow2))]
 #mainprogram---------------------------------------------------------------
+excel()
 while 1:
     start_recognizer()
